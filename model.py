1fb73ae1a29a728e23b65269a0afccd2108821bd
Cernadas <cernadas1021@gmail.com>
2019-03-25 10:57:31 -0400

    adding quanitative models

--- model.py
+++ model.py
@@ -14,8 +14,6 @@ from wrapper import get_eod_data, transform_dfs
 
 puts, calls = get_eod_data()
 df_puts, df_calls = transform_dfs(puts, calls)
-print(df_calls)
-
 
 def N(z):
     return norm.cdf(z)
@@ -30,6 +28,97 @@ def black_scholes_put_val(S,K,r,t,vol):
     d2 = d1 - (vol * np.sqrt(t))
     return N(-d2) * K * np.exp(-r * t) - N(-d1) * S
 
-def get_days_until_exp(series):
-    exp = series["expirationDate"]
-    date_str = exp.strftime("%")
+def phi(x):
+    return np.exp(-0.5 * x * x) / (sqrt(2.0 * pi))
+
+def call_delta(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    return N(d1)
+
+def put_delta(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    return N(d1) - 1.0
+
+def gamma(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    return phi(d1) / (S * vol * sqrt(t))
+
+def vega(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    return (S * phi(d1) * sqrt(t)) / 100.0
+
+def call_theta(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    d2 = d1 - (vol * np.sqrt(t))
+    theta = -((S * phi(d1) * vol) / (2.0 * np.sqrt(t))) - \
+                (r * K * np.exp(-r * t) * N(d2))
+    return theta / 365.0
+
+def put_theta(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    d2 = d1 - (vol * np.sqrt(t))
+    theta = -((S * phi(d1) * vol) / (2.0 * np.sqrt(t))) + \
+                (r * K * np.exp(-r * t) * N(-d2))
+    return theta / 365.0
+
+def call_rho(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    d2 = d1 - (vol * np.sqrt(t))
+    rho = K * t * np.exp(-r * t) * N(d2)
+    return rho / 100.0
+
+def put_rho(S,K,r,t,vol):
+    d1 = (1.0 / (vol * np.sqrt(t))) * (np.log(S / K) + (r + 0.5 * vol**2.0) * t)
+    d2 = d1 - (vol * np.sqrt(t))
+    rho = -K * t * np.exp(-r * t) * N(-d2)
+    return rho / 100.0
+
+# def call_implied_volatility_obj_func(S,K,r,t,vol,call_price):
+#     return call_price - black_scholes_call_val(S,K,r,t,vol)
+
+def get_days_until_exp(df):
+    exp = df["expDate"]
+    date_str = exp + " 23:59:59"
+    expiry = dt.datetime.strptime(date_str,"%Y-%m-%d %H:%M:%S")
+    today  = dt.datetime.today()
+    return (expiry - today).days + 1
+
+def get_time_fraction_until_exp(df):
+    exp = df["expDate"]
+    date_str = exp + " 23:59:59"
+    time_tuple = time.strptime(date_str,"%Y-%m-%d %H:%M:%S")
+    expiry_in_seconds_from_epoch = time.mktime(time_tuple)
+    right_now_in_seconds_from_epoch = time.time()
+    seconds_until_expiration = expiry_in_seconds_from_epoch - \
+    right_now_in_seconds_from_epoch
+    seconds_in_year = 31536000.0
+    return max(seconds_until_expiration / seconds_in_year, 1e-10)
+
+terms = [30, 30*3, 30*6, 30*12, 30*24, 30*36, 30*60]
+rates = [0.0001, 0.0009, 0.0032, 0.0067, 0.0097, 0.0144, 0.0184]
+
+def get_rate(df):
+    days = df["DaysUntilExp"]
+    new_terms = [i for i in range(30,(60 * 30) + 1)]
+    f = interp1d(terms, rates, kind="linear")
+    ff = f(new_terms)
+    return round(ff[max(days,30) - 30], 8)
+
+def get_mid(df):
+    bid = df["bid"]
+    ask = df["ask"]
+    last = df["last"]
+    if np.isnan(ask) or np.isnan(bid):
+        return 0.0
+    elif ask == 0.0 or bid == 0.0:
+        return last
+    else:
+        return (ask + bid) / 2.0
+
+df_calls["DaysUntilExp"] = df_calls.apply(get_days_until_exp, axis=1)
+df_calls["TimeUntilExp"] = df_calls.apply(get_time_fraction_until_exp, axis=1)
+df_calls["InterestRate"] = df_calls.apply(get_rate, axis=1)
+df_calls["Mid"] = df_calls.apply(get_mid, axis=1)
+
+print(df_calls.head(50))
+
--- wrapper.py
+++ wrapper.py
@@ -21,8 +21,8 @@ def get_eod_data(symbol = "AAPL.US", api_token = key):
 	put_lst, call_lst = list(), list()
 	for x in lst1:
 		put,call = x["options"]["PUT"], x["options"]["CALL"]
-		cols = ["ask","bid","change","changePercent","contractName","contractSize",
-		"currency","delta","lastTradeDateTime","rho","theta","gamma","intrinsicValue",
+		cols = ["change","changePercent","contractName","contractSize","currency",
+		"delta","lastTradeDateTime","rho","theta","gamma","intrinsicValue",
 		"theoretical","timeValue","updatedAt","vega","impliedVolatility"]
 		puts,calls = pd.DataFrame.from_dict(put), pd.DataFrame.from_dict(call)
 		puts.drop(cols,axis=1,inplace=True)
@@ -35,18 +35,22 @@ def get_eod_data(symbol = "AAPL.US", api_token = key):
 def transform_dfs(df_puts,df_calls):
 	df_puts.reset_index(drop=True,inplace=True)
 	df_calls.reset_index(drop=True,inplace=True)
-	cols = ["expirationDate","type","strike","inTheMoney","lastPrice","volume"]
-	to_floats = ["strike","lastPrice","volume"]
+	cols = ["expirationDate","type","strike","inTheMoney",
+			"lastPrice","bid","ask","volume"]
 	df_P,df_C = df_puts[cols], df_calls[cols]
 	convert_dict = {"strike"    : float,
 					"lastPrice" : float,
+					"bid"		: float,
+					"ask"		: float,
 					"volume"    : float}
 	df_P, df_C = df_P.astype(convert_dict), df_C.astype(convert_dict)
 	columns = {	"expirationDate": "expDate",
 				"type"			: "type",
 				"strike"		: "strike",
 				"inTheMoney"	: "itm",
-				"lastPrice" 	: "price",
+				"lastPrice" 	: "last",
+				"bid"			: "bid",
+				"ask"			: "ask",
 				"volume"		: "vol"}
 	df_P.rename(columns=columns,inplace=True)
 	df_C.rename(columns=columns,inplace=True)
