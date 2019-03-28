6e90eaa011e9d5f322a801ff63aa44e0ef8157ef
Cernadas <cernadas1021@gmail.com>
2019-03-28 15:31:20 -0400

    adding quanitative models

--- yves_model.py
+++ yves_model.py
@@ -1,3 +1,21 @@
+import numpy as np 
+from pylab import plt, mpl
 
+plt.style.use('seaborn')
+mpl.rcParams["font.family"] = "serif"
 
-import 
+def f(x):
+    return np.sin(x) + 0.5 * x
+
+def create_plot(x,y,styles,labels,axlabels):
+    plt.figure(figsize=(10,6))
+    for i in range(len(x)):
+        plt.plot(x[i],y[i],styles[i],label=labels[i])
+        plt.xlabel(axlabels[0])
+        plt.ylabel(axlabels[1])
+    plt.legend(loc=0)
+
+x = np.linspace(-2 * np.pi, 2 * np.pi, 50)
+
+create_plot([x],[f(x)],['b'],['f(x)'],['x', 'f(x)'])
+plt.show()
